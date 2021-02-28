16bd9bac1256559c98dbdddb5124bd418902d67a
romdotdog <70765447+romdotdog@users.noreply.github.com>
2021-02-28 09:13:41 -0600

    create prettier config

--- src/App/components/Markdown/index.tsx
+++ src/App/components/Markdown/index.tsx
@@ -1,37 +1,46 @@
-import * as React from 'react';
-import marked from 'marked';
-import ReactPlaceholder from 'react-placeholder/lib';
-import styled from 'styled-components';
+import * as React from "react";
+import marked from "marked";
+import ReactPlaceholder from "react-placeholder/lib";
+import styled from "styled-components";
 
-import './darkdown.css';
-import Header from './Header';
+import "./extras.css";
+import Header from "./Header";
 
 const Padding = styled.div`
-    padding: 24px;
-    max-width: 1012px;
-    margin-left: auto;
-    margin-right: auto;
-`
+	padding: 24px;
+	max-width: 1012px;
+	margin-left: auto;
+	margin-right: auto;
+`;
 
 const Markdown = () => {
-    const [markdown, setMarkdown] = React.useState("");
+	const [markdown, setMarkdown] = React.useState("");
 
-    React.useEffect(() => {
-        fetch("https://edge.rom.dog/gh/README/romdotdog")
-            .then(r => r.text())
-            .then(r => {
-                setMarkdown(r);
-            })
-    }, [])
+	React.useEffect(() => {
+		fetch("https://edge.rom.dog/gh/README/romdotdog")
+			.then((r) => r.text())
+			.then((r) => {
+				setMarkdown(r);
+			});
+	}, []);
 
-    return (
-        <Padding>
-            <ReactPlaceholder type='text' rows={16} color="#333333" showLoadingAnimation={true} ready={markdown !== ""}>
-                <Header />
-                <div className="markdown-body" dangerouslySetInnerHTML={{ __html: marked(markdown) }} />
-            </ReactPlaceholder>
-        </Padding>
-    );
+	return (
+		<Padding>
+			<ReactPlaceholder
+				type="text"
+				rows={16}
+				color="#333333"
+				showLoadingAnimation={true}
+				ready={markdown !== ""}
+			>
+				<Header />
+				<div
+					className="markdown-body"
+					dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+				/>
+			</ReactPlaceholder>
+		</Padding>
+	);
 };
 
 export default Markdown;
